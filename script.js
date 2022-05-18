fc6cdeb66561d8404ca03df5de1f30cac4f07200
Gloria <gloghe@yahoo.it>
2022-05-18 13:37:03 +0200

    array membri team

---
+++ script.js
@@ -0,0 +1,35 @@
+// arrey di oggetti (membri team)
+
+const membriTeam = 
+[
+    {
+        'nome': 'Wayne Barnett',
+        'ruolo': 'Founder & CEO',
+        'foto': 'img/wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nome': 'Angela Carrol',
+        'ruolo': 'Chief Editor',
+        'foto': 'img/angela-caroll-chief-editor.jpg'
+    },
+    {
+        'nome': 'Walter Gordon',
+        'ruolo': 'Office Manager',
+        'foto': 'img/walter-gordon-office-manager.jpg'
+    },
+    {
+        'nome': 'Angela Lopez',
+        'ruolo': 'Social Media Manager',
+        'foto': 'img/angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nome': 'Scott Estrada',
+        'ruolo': 'Developer',
+        'foto': 'img/scott-estrada-developer.jpg'
+    },   
+    {
+        'nome': 'Barbara Ramos',
+        'ruolo': 'Graphic Designer',
+        'foto': 'img/barbara-ramos-graphic-designer.jpg'
+    }     
+];
